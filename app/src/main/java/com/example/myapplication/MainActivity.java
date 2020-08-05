15c5d37ac6c2d3f2b47ec8a817b7b1c3e40c95a3
duclong09 <56545997+duclong09@users.noreply.github.com>
2020-08-05 20:30:43 +0700

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    AnimationDrawable wifiAnimation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView imageView = (ImageView) findViewById(R.id.image);
+        imageView.setBackgroundResource(R.drawable.loading);
+        wifiAnimation = (AnimationDrawable) imageView.getBackground();
+    }
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        wifiAnimation.start();
+    }
+}
